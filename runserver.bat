ef813072e7a78237774e67f51f253bc5adc10108
asrashley <alex@ashley-family.net>
2013-08-02 16:14:54 +0100

    changed appid for bulkloader

--- runserver.bat
+++ runserver.bat
@@ -1,1 +1,2 @@
-python "c:\Program Files (x86)\Google\google_appengine\dev_appserver.py" --port=9080 --host=0.0.0.0 app.yaml
+python "c:\Program Files (x86)\Google\google_appengine\dev_appserver.py" --port=9080 --host=0.0.0.0 --datastore_path=.gaedata\datastore.sql3 app.yaml 
+rem python "..\google_appengine\dev_appserver.py" --port=9080 --host=0.0.0.0 --datastore_path=.gaedata\datastore.sql3 app.yaml 
